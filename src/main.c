5797d3540df1fc5ccc0eb3d988e035facecad34d
Benjamin Davey <bld47282@gmail.com>
2021-01-28 09:37:26 +1100

    Create SDL window

--- src/main.c
+++ src/main.c
@@ -19,10 +19,32 @@ int main(int argc, char *argv[]) {
 	else {
 		filename = argv[1];
 	}
+
 	initialise(&ch8);
 
 	load_rom(&ch8, filename);
 
+	// SDL Initialisation
+
+	SDL_Window * window = NULL;
+	SDL_Surface* screen = NULL;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL error: %s\n", SDL_GetError());
+	} else {
+		window = SDL_CreateWindow("Chip8", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+				SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	}
+	if (window == NULL) {
+		printf("Window not created: %s\n", SDL_GetError());
+	} else {
+		screen = SDL_GetWindowSurface(window);
+		SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 0xFF, 0xFF, 0xFF));
+		SDL_UpdateWindowSurface(window);
+		SDL_Delay(2000);
+	}
+	
+	// Main loop
+	
 	while (1) {
 		cycle(&ch8);
 
@@ -31,5 +53,8 @@ int main(int argc, char *argv[]) {
 
 		get_key_state(&ch8);
 	}
-	return 1;
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
 }
